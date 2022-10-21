151fd9c2061b79b4f792a27252fdd3664b924347
Nikita Elfimov <me@tfk.name>
2022-10-21 21:23:34 +0500

    chore(marketplace): hoist cron expression

--- marketplace/adapters/jobs-adapter/src/jobs/write-products.job.ts
+++ marketplace/adapters/jobs-adapter/src/jobs/write-products.job.ts
@@ -1,15 +1,15 @@
-import { Logger }                   from '@atls/logger'
-import { Injectable }               from '@nestjs/common'
-import { CommandBus }               from '@nestjs/cqrs'
-import { CronExpression }           from '@nestjs/schedule'
-import { Cron }                     from '@nestjs/schedule'
+import { Logger }                             from '@atls/logger'
+import { Injectable }                         from '@nestjs/common'
+import { CommandBus }                         from '@nestjs/cqrs'
+import { Cron }                               from '@nestjs/schedule'
 
-import { WriteProductsCommand }     from '@marketplace/application-module'
-import { InjectProductsRepository } from '@marketplace/domain-module'
-import { ProductsRepository }       from '@marketplace/domain-module'
+import { WriteProductsCommand }               from '@marketplace/application-module'
+import { InjectProductsRepository }           from '@marketplace/domain-module'
+import { ProductsRepository }                 from '@marketplace/domain-module'
 
-import { InjectActiveJob }          from '../decorators'
-import { ActiveJob }                from '../enums'
+import { WRITE_PRODUCTS_JOB_CRON_EXPRESSION } from '../constants'
+import { InjectActiveJob }                    from '../decorators'
+import { ActiveJob }                          from '../enums'
 
 @Injectable()
 export class WriteProductsJob {
@@ -23,7 +23,7 @@ export class WriteProductsJob {
     private readonly commandBus: CommandBus
   ) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_6AM)
+  @Cron(WRITE_PRODUCTS_JOB_CRON_EXPRESSION!)
   async writeProducts() {
     if (this.activeJob === ActiveJob.WRITE_PRODUCTS) {
       this.#logger.info(`Job: ${this.activeJob}`)
