2965e5facf667a54be7c63346ecd848a0262a7b0
Nikita Elfimov <me@tfk.name>
2022-11-01 15:29:20 +0500

    chore(supplier): return cleaner

--- supplier/adapters/jobs-adapter/src/jobs/products-cleaner.job.ts
+++ supplier/adapters/jobs-adapter/src/jobs/products-cleaner.job.ts
@@ -1,11 +1,13 @@
-import { Logger }          from '@atls/logger'
-import { Injectable }      from '@nestjs/common'
-import { CommandBus }      from '@nestjs/cqrs'
-import { Cron }            from '@nestjs/schedule'
-import { CronExpression }  from '@nestjs/schedule'
+import { Logger }                    from '@atls/logger'
+import { Injectable }                from '@nestjs/common'
+import { CommandBus }                from '@nestjs/cqrs'
+import { Cron }                      from '@nestjs/schedule'
+import { CronExpression }            from '@nestjs/schedule'
 
-import { InjectActiveJob } from '../decorators'
-import { ActiveJob }       from '../enums'
+import { CleanStaleProductsCommand } from '@supplier/application-module'
+
+import { InjectActiveJob }           from '../decorators'
+import { ActiveJob }                 from '../enums'
 
 @Injectable()
 export class ProductsCleanerJob {
@@ -20,10 +22,9 @@ export class ProductsCleanerJob {
   @Cron(CronExpression.EVERY_DAY_AT_6AM)
   async cleanProducts() {
     if (this.activeJob === ActiveJob.SYNCHRONIZE_PRODUCTS) {
-      // TODO start when IP unbanned
-      // this.#logger.info('Started cleaning stale products')
-      //
-      // await this.commandBus.execute(new CleanStaleProductsCommand())
+      this.#logger.info('Started cleaning stale products')
+
+      await this.commandBus.execute(new CleanStaleProductsCommand())
     }
   }
 }
